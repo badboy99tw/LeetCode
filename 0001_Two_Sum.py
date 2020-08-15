c2fa401af659f6534dcf0dc890cc9840ba54d946
badboy99tw <taiwan.badboy99@gmail.com>
2020-08-16 05:38:12 +0800

    0001: runtime 27.34%, memory usage 99.89%
    Runtime: 880 ms, faster than 27.34% of Python3 online submissions for Two Sum.
    Memory Usage: 14.6 MB, less than 99.89% of Python3 online submissions for Two Sum.

---
+++ 0001_Two_Sum.py
@@ -0,0 +1,30 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+    Given nums = [2, 7, 11, 15], target = 9,
+
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+"""
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, n1 in enumerate(nums):
+            n2 = target - n1
+            try:
+                return [i, nums.index(n2, i + 1)]
+            except ValueError:
+                continue
+
+
+def test():
+    s = Solution()
+    assert s.twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert s.twoSum([3, 2, 4], 6) == [1, 2]
+    assert s.twoSum([3, 3], 6) == [0, 1]
