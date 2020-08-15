a7ac6300bad3d183364f52f1c55a1442d77a5ec5
badboy99tw <taiwan.badboy99@gmail.com>
2020-08-16 07:23:48 +0800

    0005: runtime 5.03%, memory usage 31.83%
    Runtime: 9408 ms, faster than 5.03% of Python3 online submissions for Longest Palindromic Substring.
    Memory Usage: 14 MB, less than 31.83% of Python3 online submissions for Longest Palindromic Substring.

---
+++ 0005_Longest_Palindromic_Substring.py
@@ -0,0 +1,67 @@
+"""
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+    Input: "babad"
+    Output: "bab"
+    Note: "aba" is also a valid answer.
+
+Example 2:
+
+    Input: "cbbd"
+    Output: "bb"
+"""
+import pytest
+
+
+class Solution:
+    def isPalindrome(self, s):
+        if s == "":
+            return True
+        if len(s) == 1:
+            return True
+        helfLen = int(len(s) / 2)
+        return s[:helfLen] == s[::-1][:helfLen]
+
+    def longestPalindrome(self, s: str) -> str:
+        maxPossibleLength = 1000
+
+        start = 0
+        maxLength = 0
+        for i in range(len(s)):
+            if maxLength > len(s) - i:
+                break
+            for length in range(min(maxPossibleLength, len(s)), 0, -1):
+                if i + length > len(s):
+                    continue
+                if length < maxLength:
+                    break
+                candidate = s[i : i + length]
+                if self.isPalindrome(candidate):
+                    start = i
+                    maxLength = len(candidate)
+                    break
+        return str(s[start : start + maxLength])
+
+
+@pytest.mark.timeout(1)
+def test():
+    s = Solution()
+    assert s.isPalindrome("a") is True
+    assert s.isPalindrome("bb") is True
+    assert s.isPalindrome("aba") is True
+    assert s.isPalindrome("ba") is False
+    assert s.isPalindrome("babad") is False
+    assert s.longestPalindrome("") == ""
+    assert s.longestPalindrome("a") == "a"
+    assert s.longestPalindrome("babad") in ["bab", "aba"]
+    assert s.longestPalindrome("cbbd") == "bb"
+
+    input_ = "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa"
+    expected = input_
+    assert s.longestPalindrome(input_) == expected
+
+    input_ = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    expected = input_
+    assert s.longestPalindrome(input_) == expected
