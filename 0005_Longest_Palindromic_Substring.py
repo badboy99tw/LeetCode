da9844c20f234f4825477d6a7074056f0605c154
badboy99tw <taiwan.badboy99@gmail.com>
2020-08-20 03:03:12 +0800

    0005: runtime 61.42%, memory usage 81.26%
    Runtime: 1280 ms, faster than 61.42% of Python3 online submissions for Longest Palindromic Substring.
    Memory Usage: 13.8 MB, less than 81.26% of Python3 online submissions for Longest Palindromic Substring.

--- 0005_Longest_Palindromic_Substring.py
+++ 0005_Longest_Palindromic_Substring.py
@@ -15,47 +15,81 @@ Example 2:
     Input: "cbbd"
     Output: "bb"
 """
+from typing import Optional
+
 import pytest
 
 
 class Solution:
-    def isPalindrome(self, s):
-        if len(s) in [0, 1]:
-            return True
-        helfLen = int(len(s) / 2)
-        return s[:helfLen] == s[-1 : -helfLen - 1 : -1]
+    def getLongestOddPalindrome(self, s: str, center: int) -> Optional[dict]:
+        longestPalindrome = {
+            "start": center,
+            "end": center,
+        }
+        longestHelfLength = min(center, len(s) - center - 1)
+        for i in range(1, longestHelfLength + 1):
+            if s[center - i] == s[center + i]:
+                longestPalindrome = {
+                    "start": center - i,
+                    "end": center + i,
+                }
+            else:
+                return longestPalindrome
+        return longestPalindrome
+
+    def getLongestEvenPalindrome(self, s: str, centerLeft: int) -> Optional[dict]:
+        if centerLeft + 1 >= len(s):
+            return None
+
+        if s[centerLeft] != s[centerLeft + 1]:
+            return None
+
+        longestPalindrome = {
+            "start": centerLeft,
+            "end": centerLeft + 1,
+        }
+        longestHelfLength = min(centerLeft, len(s) - centerLeft - 1 - 1)
+        for i in range(1, longestHelfLength + 1):
+            if s[centerLeft - i] == s[centerLeft + 1 + i]:
+                longestPalindrome = {
+                    "start": centerLeft - i,
+                    "end": centerLeft + 1 + i,
+                }
+            else:
+                return longestPalindrome
+        return longestPalindrome
 
     def longestPalindrome(self, s: str) -> str:
-        maxPossibleLength = 1000
+        result = None
+        resultLength = 0
 
-        start = 0
-        maxLength = 0
-        for i in range(len(s)):
-            if maxLength > len(s) - i:
-                break
-            for length in range(min(maxPossibleLength, len(s)) - i, 0, -1):
-                if length < maxLength:
-                    break
-                candidate = s[i : i + length]
-                if self.isPalindrome(candidate):
-                    start = i
-                    maxLength = len(candidate)
-                    break
-        return str(s[start : start + maxLength])
+        for center in range(len(s)):
+            odd = self.getLongestOddPalindrome(s, center)
+            even = self.getLongestEvenPalindrome(s, centerLeft=center)
+            oddLength = odd["end"] - odd["start"] + 1
+            evenLength = even["end"] - even["start"] + 1 if even else 0
+
+            if oddLength > resultLength:
+                result = odd
+                resultLength = oddLength
+            if evenLength > resultLength:
+                result = even
+                resultLength = evenLength
+
+        if result:
+            return str(s[result["start"] : result["end"] + 1])
+        else:
+            return s
 
 
 @pytest.mark.timeout(1)
 def test():
     s = Solution()
-    assert s.isPalindrome("a") is True
-    assert s.isPalindrome("bb") is True
-    assert s.isPalindrome("aba") is True
-    assert s.isPalindrome("ba") is False
-    assert s.isPalindrome("babad") is False
     assert s.longestPalindrome("") == ""
     assert s.longestPalindrome("a") == "a"
     assert s.longestPalindrome("babad") in ["bab", "aba"]
     assert s.longestPalindrome("cbbd") == "bb"
+    assert s.longestPalindrome("aaaaaa") == "aaaaaa"
 
     input_ = "babaddtattarrattatddetartrateedredividerb"
     expected = "ddtattarrattatdd"
